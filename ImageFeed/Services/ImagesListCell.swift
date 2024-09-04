66c29ec028672b17abcdc04f8154857e76abb690
David Gordienko <grash002@yandex.ru>
2024-09-04 22:29:56 +0300

    Creating a feed

---
+++ ImageFeed/Services/ImagesListCell.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    
+    static let reuseIdentifier = "ImagesListCell"
+    
+    // MARK: - @IBOutlet
+    
+    @IBOutlet var cellLabel: UILabel!
+    @IBOutlet var cellLikeButton: UIButton!
+    @IBOutlet var cellImageView: UIImageView!
+    
+    // MARK: - Private properties
+    
+    private var likeButtonTapped = false
+    
+    // MARK: - @IBAction
+    
+    @IBAction func likeButtonDidTap(_ sender: Any) {
+        likeButtonTapped = !likeButtonTapped
+        let likeButtonImage = likeButtonTapped ? UIImage(named: "Active") : UIImage(named: "No Active")
+        cellLikeButton.setImage(likeButtonImage, for: .normal)
+    }
+    
+}
